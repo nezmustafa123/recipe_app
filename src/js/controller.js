25995f0e216eb73772eb5fb83d00b8189e00bf4d
Nez Mustafa <nezmustafa123@gmail.com>
2021-11-23 21:35:11 +0000

    loading recipes from api call

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -8,6 +8,42 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+// https:forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+console.log("Test");
+
+const showRecipe = async function () {
+  //await promise inside async function
+  try {
+    const res = await fetch(
+      //use url to get exact recipe returns a promise
+      // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc990`
+    );
+    const data = await res.json(); //returns another promise
+    //data from serer, ok property is coming from response istaself
+    // if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    //invalid id 400
+    console.log(res);
+    console.log(data);
+    //create new object get rid of underscores
+    let { recipe } = data.data; //recipe object destructure it
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    //error being caught here
+    alert(err);
+  }
+};
+
+showRecipe();
