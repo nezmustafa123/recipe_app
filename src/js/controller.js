9f2101d0ba9d380f3ac0708dd5475a0533fa7111
Nez Mustafa <nezmustafa123@gmail.com>
2021-11-24 19:31:56 +0000

    rendering the recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,7 @@
+// import icons from "../img/icons.svg"; //Parcel 1
+import icons from "url:../img/icons.svg"; //Parcel 2 import icons variable
+console.log(icons); //path to the file
+
 const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
@@ -11,11 +15,26 @@ const timeout = function (s) {
 // https:forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-console.log("Test");
+// console.log("Test");
+
+const renderSpinner = function (parentEl) {
+  //attach to any parent element passed in here
+  const markup = `
+<div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+ </div>
+`;
+  parentEl.innerHTML = "";
+  parentEl.insertAdjacentHTML("afterbegin", markup);
+};
 
 const showRecipe = async function () {
   //await promise inside async function
   try {
+    //loading recipe    //render spinner
+    renderSpinner(recipeContainer);
     const res = await fetch(
       //use url to get exact recipe returns a promise
       // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
@@ -40,6 +59,106 @@ const showRecipe = async function () {
       ingredients: recipe.ingredients,
     };
     console.log(recipe);
+
+    // 2_.Rendering recipe
+
+    const markup = `<figure class="recipe__fig">
+    <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${recipe.title}</span>
+    </h1>
+  </figure>
+
+  <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${
+        recipe.cookingTime
+      }</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${
+        recipe.servings
+      }</span>
+      <span class="recipe__info-text">servings</span>
+
+      <div class="recipe__info-buttons">
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-minus-circle"></use>
+          </svg>
+        </button>
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="${icons}#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="${icons}#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+  </div>
+
+  <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+    ${recipe.ingredients
+      .map((ing) => {
+        //return string of html inject it in the ingredient list ul
+        return `<li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="src/img/icons.svg#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>`;
+        //transform array of strings into one big string using join
+      })
+      .join("")}
+  </div>
+
+  <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${
+        recipe.publisher
+      }</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${recipe.sourceUrl}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-right"></use>
+      </svg>
+    </a>
+  </div>`;
+
+    recipeContainer.innerHTML = "";
+
+    recipeContainer.insertAdjacentHTML("afterbegin", markup);
   } catch (err) {
     //error being caught here
     alert(err);
